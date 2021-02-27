06bf026b35305717eb2297a529070869c51c8496
Alan Vekselman <avekselman@itba.edu.ar>
2021-02-26 22:54:54 -0300

    Minor changes.

--- Code/templates/js/Mouse.js
+++ Code/templates/js/Mouse.js
@@ -1,8 +1,7 @@
 let joystick = document.getElementById("joystick");
-let midsize = 11 / 100 * window.innerHeight/2;
 
 const centerX = window.innerWidth * 0.5;
-const centerY = window.innerHeight * 695/1000;
+const centerY = window.innerHeight * 750/1000;
 const radius = 165/1000 * window.innerHeight;
 const radius_ball = 55 /1000 * window.innerHeight;
 
@@ -19,17 +18,13 @@ function move(e) {
     if (distAbs > radius){
         let arg = Math.atan2(distY, distX);
 
-        posx = centerX + Math.floor(Math.cos(arg) * radius) - radius_ball;
-        posy = centerY + Math.floor(Math.sin(arg) * radius)
-
-        if (posy > centerY - 2 * radius_ball) {
-            posy -= radius_ball * Math.abs(Math.cos(arg));
-        }
+        posx = centerX + Math.cos(arg) * radius - radius_ball;
+        posy = centerY + Math.sin(arg) * radius - radius_ball;
     }
     else{
 
-        posx = (e.touches[0].clientX-midsize);
-        posy = (e.touches[0].clientY-midsize);
+        posx = (e.touches[0].clientX-radius_ball);
+        posy = (e.touches[0].clientY-radius_ball);
 
     }
 
@@ -37,7 +32,7 @@ function move(e) {
     joystick.style.top = posy.toString();
 
     let porc_x = (posx - centerX) / radius;
-    let porc_y = (posy - centerY) / radius;
+    let porc_y = (posy - centerY + radius_ball) / radius;
     myFunc(mouse + '-x=' + porc_x.toString() + 'y=' + porc_y.toString());
 }
 
