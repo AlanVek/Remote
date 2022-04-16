56ee52555536a584878ea7e3fc0a20309bd20d1f
Alan Vekselman <avekselman@novo.space>
2022-04-16 03:21:09 -0300

    Code/main.py -- Minor changes + Linux support

--- Code/main.py
+++ Code/main.py
@@ -1,32 +1,52 @@
 # Networking
 from http.server import HTTPServer
-from socket import gethostname, gethostbyname
-from subprocess import Popen, PIPE
-import re
+import platform
+
+p = platform.system()
 
 # Handler
-from Request_Handler.Handler import RHandler, update_ip, resource_path, sys
+from Request_Handler.Handler import RHandler, update_ip, resource_path, sys, join
+
+if p == 'Windows':
+    from socket import gethostname, gethostbyname
+    from subprocess import Popen, PIPE
+    import re
+
+elif p == 'Linux':
+    from subprocess import check_output
+    import platform
+
+else: sys.exit()
 
 def print_instructions():
-    with open (resource_path("Code\\Instructions.txt"), 'rb') as file: text = file.read()
+    with open (resource_path(join("Code", "Instructions.txt")), 'rb') as file: text = file.read()
     print(text.decode('utf-8'))
 
 def get_network_name():
-    netshcmd = Popen('Netsh WLAN show interfaces', shell=True, stderr=PIPE, stdout=PIPE)
-    output, errors = netshcmd.communicate()
-    if errors: sys.exit(errors)
-    else:
-        try:
-            output = output.decode('utf-8')
-            wifinet = dict(map(str.strip, re.split('\s+:\s+', i)) for i in output.strip().splitlines() if i.startswith('    '))['Profile']
-        except: wifinet = 'Network Not Found'
+    if p == 'Windows':
+        netshcmd = Popen('Netsh WLAN show interfaces', shell=True, stderr=PIPE, stdout=PIPE)
+        output, errors = netshcmd.communicate()
+        if errors: sys.exit(errors)
+        else:
+            try:
+                output = output.decode('utf-8')
+                wifinet = dict(map(str.strip, re.split('\s+:\s+', i)) for i in output.strip().splitlines() if i.startswith('    '))['Profile']
+            except: wifinet = 'Network Not Found'
+        return wifinet
+    elif p == 'Linux':
+        s = check_output(["iwgetid"]).decode('utf-8').split('ESSID:')[1]
+        return s[1:s.rfind('"')]
+    else: return 'Network Not Found'
 
-    return wifinet
+def get_ip():
+    if p == 'Windows': return gethostbyname(gethostname())
+    elif p == 'Linux': return check_output(["hostname", "-I"]).decode('utf-8').split(' ')[0]
+    else: return None
 
-if __name__ == '__main__':
+def main():
 
     # Gets local IP
-    myIP = gethostbyname(gethostname())
+    myIP = get_ip()
     RHandler.IP = myIP
     update_ip(RHandler)
 
@@ -38,6 +58,11 @@ if __name__ == '__main__':
     server = HTTPServer(server_address=('', 8000), RequestHandlerClass=RHandler)
 
     # Loops until 'Exit' is pressed
-    while RHandler.running: server.handle_request()
+    try:
+        while RHandler.running: server.handle_request()
+    except KeyboardInterrupt: pass
+
     server.server_close()
 
+if __name__ == '__main__':
+    main()
